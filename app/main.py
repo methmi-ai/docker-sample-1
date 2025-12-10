7e2423c195d046bd97b30b82abe550538aebe477
Sazul19 <ss.senarathne0919@gmail.com>
2025-12-10 16:31:44 +0530

    TODO:post request from ui need to be tested

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 
 app = FastAPI(title="Student Grade Predictor")
 
-model = joblib.load("app/grade_model.pkl")
+model = joblib.load("grade_model.pkl")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # In production, specify your domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class StudyHours(BaseModel):
     hours: float
 
--- app/model.py
+++ app/model.py
@@ -3,14 +3,14 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 def train_model():
-    data = pd.read_csv("app/grades.csv")
+    data = pd.read_csv("grades.csv")
     X = data[['hours_studied']]
     y = data['final_grade']
 
     model = LinearRegression()
     model.fit(X, y)
 
-    joblib.dump(model, "app/grade_model.pkl")
+    joblib.dump(model, "grade_model.pkl")
     print(" Model trained and saved successfully!")
 
 if __name__ == "__main__":
